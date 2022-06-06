ed2a40ea3b243881444673cf29e3413f61fb844a
BrandonSandovalUwU <brandon.sandovalgg@gmail.com>
2022-06-06 02:29:01 -0400

    database

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'node20_mysql'
+    host: 'ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'homzv95p61fzbqmv',
+    password: 'qh07tjyec75joxn0',
+    database: 'wkacna8xtoi55731'
 });
 
 module.exports = connection;
